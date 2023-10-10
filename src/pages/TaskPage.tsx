7b88c5f5a0a7dd4951eba0e1f958244f24cc1efa
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-10 21:34:57 +0300

    build: Install redux

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -3,11 +3,20 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import { AddOrUpdateButton } from '../components/AddOrUpdateButton'
+import { useState } from 'react'
+import { uid } from 'uid';
 
 export const TaskPage = ({ editMode }: { editMode?: boolean}) => {
+  const [formData, setFormData] = useState({
+    id: uid(),
+    completion: false,
+
+  })
   return (
     <>
-      <h3>{editMode ? 'Update Your Task' : 'Create a Task'}</h3>
+      <h3 style={{ textAlign: 'center' }}>
+        {editMode ? 'Update Your Task' : 'Create a Task'}
+      </h3>
       <Form>
         <Col className="mb-3">
           <Form.Group as={Row} md="6" className="mb-3">
@@ -15,6 +24,7 @@ export const TaskPage = ({ editMode }: { editMode?: boolean}) => {
             <Form.Control
               required
               type="text"
+              name="task"
               placeholder="Enter a task"
             ></Form.Control>
           </Form.Group>
@@ -23,21 +33,24 @@ export const TaskPage = ({ editMode }: { editMode?: boolean}) => {
             <Form.Control
               required
               type="text"
+              name="description"
               placeholder="Enter a description"
             ></Form.Control>
           </Form.Group>
-          { editMode ? (<Form.Group as={Col} md="6">
-            <Form.Label className="ml-0">Completion: </Form.Label>
-            <Form.Check
-              type="radio"
-              id={`default-radio`}
-              name="completion"
-              label={'done'}
-            />
-          </Form.Group>) : null
-          }
+          {editMode ? (
+            <Form.Group as={Col} md="6">
+              <Form.Label className="ml-0">Completion: </Form.Label>
+              <Form.Check
+                type="radio"
+                name="completion"
+                label={'done'}
+              />
+            </Form.Group>
+          ) : null}
         </Col>
-        <AddOrUpdateButton title={editMode ? "Update your task" : "Add a new task"}/>
+        <AddOrUpdateButton
+          title={editMode ? 'Update your task' : 'Add a new task'}
+        />
       </Form>
     </>
   )
