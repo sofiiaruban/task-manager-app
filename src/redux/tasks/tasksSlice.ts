de5bacf9da0cf0b1f411b01e6df6ee430701a6eb
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-10 22:20:42 +0300

    feat: Add configure store

---
+++ src/redux/tasks/tasksSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../store'
+
+interface Task {
+  id: string,
+  title: string,
+  description: string,
+  completion: boolean,
+}
+const initialState: Task[] = []
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action: PayloadAction<Task>) => {
+      state.push(action.payload)
+    },
+    deleteTask: (state, action: PayloadAction<string>) => {
+      return state.filter((task) => task.id !== action.payload)
+    },
+    editTask: (state, action: PayloadAction<Task>) => {
+      const { id } = action.payload
+      const existingTaskIndex = state.findIndex((task) => task.id === id)
+
+      if (existingTaskIndex !== -1) {
+        state[existingTaskIndex] = action.payload 
+      }
+    }
+  }
+}) 
+export const { addTask, deleteTask, editTask } = tasksSlice.actions
+export const selectTasks = (state: RootState) => state.tasks
+export default tasksSlice.reducer
