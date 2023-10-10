f37aba4a2d3c76c03ad7c8a4397071d5b3fde6c2
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-10 19:17:14 +0300

    bugfix: Fix adjust  bootstrap styles bug

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -2,5 +2,5 @@
 import Button from 'react-bootstrap/Button'
 
 export const AddButton = () => {
-  return <Button variant="primary">Add a new task</Button>
+  return <Button variant="info">Add a new task</Button>
 }
